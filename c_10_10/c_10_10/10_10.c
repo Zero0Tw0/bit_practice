8dc55e23f1e1dc85d756466bebfb66aa34ded8e4
Zero0Tw0 <1464001349@qq.com>
2021-10-10 23:46:17 +0800

    c语言09_07练习

---
+++ c_10_10/c_10_10/10_10.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//17.04 ÏûÊ§µÄÊı×Ö
+int missingNumber(int* nums, int numsSize) {
+    int ans = 0;
+    int i = 0;
+    for (i = 0; i < numsSize; i++)
+    {
+        ans = ans ^ i ^ nums[i];
+    }
+    ans ^= i;
+    return ans;
+}
