b81c14c9dc06fd05df3d9482cfd3c8d3132ab2ac
Zero0Tw0 <1464001349@qq.com>
2021-11-10 22:54:09 +0800

    c语言11_10练习

---
+++ c_11_10/c_11_10/11_10.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//104. 二叉树的最大深度
+//
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+    int leftdepth = maxDepth(root->left);
+    int rightdepth = maxDepth(root->right);
+    return leftdepth > rightdepth ? leftdepth + 1 : rightdepth + 1;
+}
